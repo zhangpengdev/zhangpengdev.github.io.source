1f8593565689ac4819f0b08877efd45f252e0575
zhangpengdev <peng_dev@163.com>
2021-11-16 21:40:23 +0800

    commit_2021年 11月 16日 星期二 21:40:23 CST

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+# /bin/bash
+git checkout main
+git add .
+git commit -a -m "commit_$(date)"
+git push
---
+++ push_code.sh
@@ -0,0 +1,6 @@
+# /bin/bash
+
+git checkout main
+git add .
+git commit -a -m "commit_$(date)"
+git push
